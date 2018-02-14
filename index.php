45203d2d32d20a961c3e9d12a13550c6abb5377f
balpreetpunia <mebalpreetpunia@gmail.com>
2018-02-14 18:02:58 -0500

    Commented event preventer (useful when using barcode scanner)

--- index.php
+++ index.php
@@ -105,10 +105,10 @@
     ?>
 </script>
 <script>
-    document.addEventListener('keydown', function(event) {
+    /*document.addEventListener('keydown', function(event) {
         if( event.keyCode == 13 || event.keyCode == 17 || event.keyCode == 74 )
             event.preventDefault();
-    });
+    });*/
 </script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
