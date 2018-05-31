da2a4db7b3ccc16804ef2cd8932387025e04206d
balpreetpunia <mebalpreetpunia@gmail.com>
2018-05-31 16:41:35 -0400

    Added code to generate duplicate.

--- multiple.php
+++ multiple.php
@@ -194,6 +194,31 @@ if (isset($_POST["multiple"])) {
         }
     ?>
 </script>
+<?php if(!empty($duplicates)) :?>
+<form name="duplicate_form" id="duplicate_form" method="post" action="">
+    <?php
+        $form_duplicates="";
+        foreach ($duplicates as $key => $val){
+            if ($val > 1){
+                for ($i=0;$i<$val;$i++){
+                    $form_duplicates .= "$key,";
+                }
+            }
+            else{
+                $form_duplicates .= "$key,";
+            }
+    }
+    ?>
+    <input type="hidden" name="multiple" value="<?=$form_duplicates?>">
+</form>
+<script>
+    function submit_form()
+    {
+        document.duplicate_form.submit();
+    }
+    document.getElementById("generate_duplicate").addEventListener("click", submit_form, false);
+</script>
+<?php endif;?>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
