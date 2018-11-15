45713f22658fe719c2546de086ae3e84df1c11ca
balpreetpunia <mebalpreetpunia@gmail.com>
2018-11-15 11:39:19 -0500

    Updated connection string.

--- shared/connect.php
+++ shared/connect.php
@@ -1,11 +1,9 @@
 <?php
+try{
 
-    try{
-        $dbh = new PDO( "mysql:host=localhost;dbname=bartag", "root", "" );
-        $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }
-    catch (PDOException $e){
-        echo 'Database connection failed: ' . $e->getMessage();
-    }
 
-?>
+    $dbh = new PDO("mysql:host=den1.mysql5.gear.host;dbname=teletimeinven","teletimeinven","Fr47flr_?293");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo 'Database Connection failed: ' . $e->getMessage();
+}
